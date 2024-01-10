a1b94986a10fc3847fbb42b5a27d21fc04d88028
vlad636 <64536467+vlad636@users.noreply.github.com>
2024-01-10 11:28:13 +0200

    Restructure

--- index.js
+++ index.js
@@ -1,74 +1,73 @@
-function getRange() {
-    // Valoare range
-    const value = document.querySelector("#value");
-    const input = document.querySelector("#pi_input");
-    value.textContent = input.value;
-    input.addEventListener("input", (event) => {
-        value.textContent = event.target.value;
-    });
+// Function to update the displayed age value
+function updateAgeValue() {
+    var ageInput = document.getElementById('varsta');
+    var outputSpan = document.getElementById('ageOutput');
+
+    // Update the displayed value
+    outputSpan.textContent = ageInput.value;
 }
 
 
-//drag&drop
-    function dragOver(event) {
-      event.preventDefault();
-    }
 
-    function drop(event) {
-      event.preventDefault();
-      // Perform actions when the element is dropped
-      console.log('Element dropped!');
-    }
+//drag&drop
+function dragOver(event) {
+    event.preventDefault();
+}
+
+function drop(event) {
+    event.preventDefault();
+    // Perform actions when the element is dropped
+    console.log('Element dropped!');
+}
 //const draggableElement = document.getElementById('draggableElement');
 
 //draggableElement.addEventListener('dragstart', (event) => {
-   // event.dataTransfer.setData('text/plain', 'Dragged Element');
+// event.dataTransfer.setData('text/plain', 'Dragged Element');
 //});
 
 
-
-// Get the canvas element using its id
+//se preia elementul de canvas
 var canvas = document.getElementById('myCanvas');
 
-// Get the 2D rendering context
+//facem un context pe care o sa l folosim mai departe pt diferite actiuni
 var ctx = canvas.getContext('2d');
 var ctx2 = canvas.getContext('2d');
 
-//Draw a triangle
+//incepem trasarea liniilor pt un triunghi
 ctx.beginPath();
 ctx.moveTo(150, 150);
 ctx.lineTo(100, 300);
 ctx.lineTo(300, 300);
 ctx.closePath();
 
-// the outline
+//linia exterioara a triunghiului
 ctx.lineWidth = 10;
-ctx.strokeStyle = '#666666';
+ctx.strokeStyle = 'black';
 ctx.stroke();
 
-// the fill color
-ctx.fillStyle = "#FFCC00";
+//culoarea din triunghi
+ctx.fillStyle = "red";
 ctx.fill();
 
-// Draw a rectangle on the canvas
-ctx2.fillStyle = '#3498db'; // Set the fill color to a blue shade
+//desenam un dreptunghi
+ctx2.fillStyle = 'blue';
 ctx2.fillRect(50, 40, 200, 100); // Draw a rectangle at (50, 50) with a width of 200 and height of 100
 
-// Draw text on the canvas
+//adaugam scris in interiorul dreptunghiului
 ctx2.font = '20px Arial';
 ctx2.fillStyle = '#fff'; // Set the fill color to white
-ctx2.fillText('Hello, Canvas!', 75, 100); // Draw the text at (70, 100)
+ctx2.fillText('Hello, Canvas!', 75, 100); // Draw the text at (75, 100)
 
 
 
 //Geolocatia
-// Function to get the current date and time
+//preia data de azi si o transforma in string
 function getCurrentDateTime() {
     var now = new Date();
     return now.toLocaleString();
 }
 
-// Function to get the user's location
+//preia locatia userului, iar in cazul in care geolocatia nu e suportata de browser trimite un mesaj pe elementul cu id ul locationDateTime
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
@@ -77,6 +76,7 @@ function getLocation() {
     }
 }
 
+//preia datele din cele doua functii anterioare si le pune in pagina, coordonatele cu fix 4 zecimale
 function showPosition(position) {
     var locationDateTimeDiv = document.getElementById("locationDateTime");
     var locationInfo = "Your location: Latitude " + position.coords.latitude.toFixed(4) + ", Longitude " + position.coords.longitude.toFixed(4);
@@ -84,5 +84,5 @@ function showPosition(position) {
 
     locationDateTimeDiv.innerHTML = "<p>" + locationInfo + "</p><p>" + dateTimeInfo + "</p>";
 }
-// Call the function to get the user's location
+//se apeleaza functia de getLocation
 getLocation();
